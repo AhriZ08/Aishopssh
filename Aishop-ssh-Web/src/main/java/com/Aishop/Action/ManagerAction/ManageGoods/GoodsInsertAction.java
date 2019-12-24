eec3836a5d1ed597df7ae93061f9752dbb3b3d58
AhriZ08 <1061747368@qq.com>
2019-12-24 22:19:04 +0800

    修改ManagerGoodsAction

--- Aishop-ssh-Web/src/main/java/com/Aishop/Action/ManagerAction/ManageGoods/GoodsInsertAction.java
+++ Aishop-ssh-Web/src/main/java/com/Aishop/Action/ManagerAction/ManageGoods/GoodsInsertAction.java
@@ -2,6 +2,9 @@ package com.Aishop.Action.ManagerAction.ManageGoods;
 
 import com.Aishopssh.Dao.Common.MyTools;
 import com.Aishopssh.Entites.Goods;
+import com.Aishopssh.Entites.Type1;
+import com.Aishopssh.Entites.Type2;
+import com.Aishopssh.Entites.Type3;
 import com.Aishopssh.Imp.ManagerServiceImp.ManageGoodsServiceImp;
 import com.opensymphony.xwork2.ActionSupport;
 import org.apache.struts2.interceptor.ServletRequestAware;
@@ -13,6 +16,7 @@ import org.springframework.stereotype.Controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,9 +37,17 @@ public class GoodsInsertAction extends ActionSupport implements ServletResponseA
         goods.setAddtime(request.getParameter("addtime"));
         goods.setMarketPirce(request.getParameter("shichangjia"));
         goods.setMemberPrice(request.getParameter("huiyuanjia"));
-        goods.setTypeIdLevel1(MyTools.strToint(request.getParameter("type1")));
-        goods.setTypeIdLevel2(MyTools.strToint(request.getParameter("type2")));
-        goods.setTypeIdLevel3(MyTools.strToint(request.getParameter("type3")));
+
+        Type1 type1 = new Type1();
+        type1.setId(MyTools.strToint(request.getParameter("type1")));
+        goods.setType1(type1);
+        Type2 type2 = new Type2();
+        type2.setId(MyTools.strToint(request.getParameter("type2")));
+        goods.setType2(type2);
+        Type3 type3 = new Type3();
+        type3.setId(MyTools.strToint(request.getParameter("type3")));
+        goods.setType3(type3);
+
         goods.setLevel(request.getParameter("dengji"));
         goods.setBrand(request.getParameter("pinpai"));
         goods.setModel(request.getParameter("xinghao"));
